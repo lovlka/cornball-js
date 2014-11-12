35d17978bcb11ea220054f42285941161cceae7f
Victor Stodell <victor@stodell.se>
2014-11-12 17:22:01 +0100

    Added interact.js to replace jquery ui drag/drop

--- app/views/card/card-view.js
+++ app/views/card/card-view.js
@@ -1,4 +1,6 @@
 module.exports = Chaplin.View.extend({
+   x: 0,
+   y: 0,
    noWrap: true,
    autoRender: true,
 
@@ -18,11 +20,32 @@ module.exports = Chaplin.View.extend({
    render: function() {
       Chaplin.View.prototype.render.call(this, arguments);
       var view = this;
-      this.$el.draggable({
-         revert: true,
-         stop: function() {
-            view.publishEvent('card:move', view);
-         }
-      });
+
+      interact(this.el)
+         .draggable({
+            onmove: _.bind(this.dragMove, this),
+            onend: _.bind(this.dragEnd, this)
+         });
+   },
+
+   dragMove: function(event) {
+      this.x += event.dx;
+      this.y += event.dy;
+      this.setPosition(event);
+   },
+
+   dragEnd: function(event) {
+      this.dragReset(event);
+      this.publishEvent('card:move', this);
+   },
+
+   dragReset: function(event) {
+      this.x = 0;
+      this.y = 0;
+      this.setPosition(event);
+   },
+
+   setPosition: function(event) {
+      event.target.style.webkitTransform = event.target.style.transform = 'translate(' + this.x + 'px, ' + this.y + 'px)';
    }
 });
