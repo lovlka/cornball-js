1a36b6a2d00f01c8deceb57d942ea35284f617aa
Victor Stodell <victor@stodell.se>
2014-11-23 00:57:02 +0100

    Added specific templates for game over/round over

--- app/views/gameover/gameover-view.js
+++ app/views/gameover/gameover-view.js
@@ -2,8 +2,14 @@ var ModalView = require('views/modal/modal-view');
 
 module.exports = ModalView.extend({
    initialize: function (options) {
-      this.title = "Slut på omgång " + this.model.get('round');
-      this.bodyTemplate = require('views/gameover/gameover');
+      if(this.model.get('round') < this.model.get('rounds')) {
+         this.title = "Slut på omgång " + this.model.get('round');
+         this.bodyTemplate = require('views/gameover/roundover');
+      }
+      else {
+         this.title = "Spelet slut!";
+         this.bodyTemplate = require('views/gameover/gameover');
+      }
       ModalView.prototype.initialize.call(this, arguments);
    }
 });
