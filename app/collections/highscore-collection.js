0ce7f2f25b9c4d26df99fdc6d850435b43bd21c0
Victor Stodell <victor@stodell.se>
2014-11-23 12:30:51 +0100

    Added high score in win dialog

--- app/collections/highscore-collection.js
+++ app/collections/highscore-collection.js
@@ -1,3 +1,16 @@
 module.exports = Chaplin.Collection.extend({
-   url: '/api/highscores/10'
+   url: '/api/highscores/10',
+
+   isHighscore: function(score) {
+      if(this.length < 10) {
+         return true;
+      }
+      var highscore = false;
+      _.each(this.models, function(model) {
+         if(score > model.get('value')) {
+            highscore = true;
+         }
+      });
+      return highscore;
+   }
 });
--- app/views/gamewin/gamewin-view.js
+++ app/views/gamewin/gamewin-view.js
@@ -1,9 +1,33 @@
 var ModalView = require('views/modal/modal-view');
+var HighscoreCollection = require('collections/highscore-collection');
 
 module.exports = ModalView.extend({
    initialize: function (options) {
       this.title = "Grattis!";
       this.bodyTemplate = require('views/gamewin/gamewin');
+      this.highscores = new HighscoreCollection();
       ModalView.prototype.initialize.call(this, arguments);
+
+      this.model.unset('highscores');
+      this.model.unset('ishighscore');
+   },
+
+   render: function () {
+      ModalView.prototype.render.call(this, arguments);
+      this.fetchHighScore();
+   },
+
+   fetchHighScore: function() {
+      this.showSpinner();
+      this.highscores.fetch({
+         success: _.bind(this.fetchSuccess, this)
+      });
+   },
+
+   fetchSuccess: function() {
+      this.model.set('highscores', this.highscores);
+      this.model.set('ishighscore', this.highscores.isHighscore(this.model.get('score')));
+      console.log('redering!', this.model);
+      this.renderContent();
    }
 });
