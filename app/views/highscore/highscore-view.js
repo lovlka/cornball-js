5e47d685b3ee6aff46abfbc3e3bfdf4cee09e3a9
Victor Stodell <victor@stodell.se>
2014-12-18 16:00:51 +0100

    Added navigation for previous and next month in high score

--- app/views/highscore/highscore-view.js
+++ app/views/highscore/highscore-view.js
@@ -8,7 +8,27 @@ module.exports = ModalView.extend({
       this.bodyTemplate = require('views/highscore/highscore');
       this.allTime = new HighscoreCollection();
       this.month = new HighscoreCollection();
+      this.currentMonth = moment().date(1);
       ModalView.prototype.initialize.call(this, arguments);
+   },
+
+   events: {
+      'click .js-prev': 'previousMonth',
+      'click .js-next': 'nextMonth'
+   },
+
+   previousMonth: function (e) {
+      e.preventDefault();
+      this.currentMonth.subtract(1, 'months');
+      this.showSpinner();
+      this.fetchMonth();
+   },
+
+   nextMonth: function (e) {
+      e.preventDefault();
+      this.currentMonth.add(1, 'months');
+      this.showSpinner();
+      this.fetchMonth();
    },
 
    render: function () {
@@ -24,13 +44,15 @@ module.exports = ModalView.extend({
    },
 
    fetchMonth: function() {
-      this.month.startDate = moment().date(1).format('YYYY-MM-DD');
+      this.month.startDate = this.currentMonth.format('YYYY-MM-DD');
+      this.month.endDate = this.currentMonth.clone().add('months', 1).date(0).format('YYYY-MM-DD');
       this.month.fetch({
          success: _.bind(this.fetchSuccess, this)
       });
    },
 
    fetchSuccess: function() {
+      this.model.set('monthHeader', i18n.t('highscore.month', this.currentMonth.format('MMMM YYYY')));
       this.model.set('allTime', this.allTime);
       this.model.set('month', this.month);
       this.renderContent();
